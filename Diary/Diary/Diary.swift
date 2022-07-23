74a715d402eeec198fb1ed4c75f9f8130582aa7c
Leejeongchan1733026 <cksqls288@gmail.com>
2022-07-23 19:00:42 +0900

    일기 어플 기능

---
+++ Diary/Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by 이정찬 on 2022/07/23.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
---
+++ Diary/Diary/StarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StarViewController.swift
+//  Diary
+//
+//  Created by 이정찬 on 2022/07/23.
+//
+
+import UIKit
+
+class StarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
