f0d24763b5eb5b99ab94c7ec294d594eeaa039e4
Leejeongchan1733026 <cksqls288@gmail.com>
2022-07-28 12:01:29 +0900

    weather api app

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -9,10 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var maxTempLable: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func tapFetchWeatherButton(_ sender: UIButton) {
+        if let cityName = self.cityNameTextField.text{
+            self.getCurrentWeather(cityName: cityName)
+            self.view.endEditing(true)
+        }
+    }
+    
+    func getCurrentWeather(cityName: String) {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=3f6734c58e404edff66a4bfc073a99a9") else { return }
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url){ data, response, error in
+            guard let data = data, error == nil else { return }
+            let decoder = JSONDecoder()
+            let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data)
+            debugPrint(data)
+        }.resume()
+    }
 }
 
